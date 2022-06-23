21b620397e75ea8c1a35c7e8f2070d724f2e62ce
Animashaun olasunkanmi <animashaunstephen@gmail.com>
2022-06-23 09:23:50 +0100

    first javascript commit

---
+++ about.js
@@ -0,0 +1,5 @@
+var myName = "Animashaun Ola";
+var height = "1.89 metres";
+var country = "Nigeria";
+alert("My name is " + myName + ", I am " + height + " tall, " + "I am from " + country);
+console.log("My name is " + myName + ", I am " + height + " tall, " + "I am from " + country);
